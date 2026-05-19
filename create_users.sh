ab7528b9f9a5e7ad6ac588e37cdc01c7060ce33d
algotmalmborg-droid <algot.malmborg@gmail.com>
2026-05-19 11:14:26 +0200

    Färdig test 2

--- create_users.sh
+++ create_users.sh
@@ -12,25 +12,23 @@ fi
 for user in "$@"; do
   useradd -m "$user"
   echo "USER '$user' created."
-
   home_dir="/home/$user"
-
   mkdir -p "$home_dir/Documents"
   mkdir -p "$home_dir/Downloads"
   mkdir -p "$home_dir/Work"
-
   chmod 700 "$home_dir/Documents"
   chmod 700 "$home_dir/Downloads"
   chmod 700 "$home_dir/Work"
-
   chown -R "$user":"$user" "$home_dir"
+done
 
+for user in "$@"; do
+  home_dir="/home/$user"
   welcome_file="$home_dir/welcome.txt"
-
   echo "Välkommen $user" > "$welcome_file"
   echo "" >> "$welcome_file"
   echo "Andra användare i systemet:" >> "$welcome_file"
- 
+
   while IFS=: read -r existing_user _ uid _; do
     if [ "$uid" -ge 1000 ] && [ "$existing_user" != "$user" ]; then
        echo "- $existing_user" >> "$welcome_file"
